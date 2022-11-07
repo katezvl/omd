6bd6752e642bd7e631e9c5b46e3a1955b80f6a13
katezvl <ezavyalova07@gmail.com>
2022-11-08 00:48:41 +0300

    hw4

--- hw4.py
+++ hw4.py
@@ -1,31 +1,45 @@
 import json
+from keyword import iskeyword
 
 
 class BaseAdvert:
-    price = 0
     repr_color_code = 33
 
-    def __init__(self):
-        pass
+    def __init__(self, ads):
+        self.add_qualities(ads)
 
-    def adder(self, ads):
+    def add_qualities(self, ads):
+        if 'price' not in ads:
+            ads['price'] = 0
         for key, value in ads.items():
+            if iskeyword(key):
+                key = key + '_'
             setattr(self, key, value)
-            if key == 'location':
-                for a, b in ads['location'].items():
-                    setattr(self, a, b)
-            if self.price < 0:
+            if isinstance(value, dict):
+                setattr(self, 'location', BaseAdvert(ads['location']))
+            if key == 'price' and value < 0:
                 raise ValueError('must be >= 0')
 
     def __repr__(self):
         return f'{self.title} | {self.price} ₽'
 
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError('must be >=0')
+        self._price = value
+
 
 class ColourMixin:
+    repr_color_code = 33
 
     def __repr__(self):
         text = super().__repr__()
-        return f'\033[1;{super().repr_color_code};40m {text}'
+        return f'\033[1;{self.repr_color_code};40m {text}'
 
 
 class Advert(ColourMixin, BaseAdvert):
@@ -42,8 +56,7 @@ if __name__ == '__main__':
                       }
     }"""
     lesson = json.loads(lesson_str)
-    lesson_ad = Advert()
-    lesson_ad.adder(lesson)
+    lesson_ad = Advert(lesson)
     print(lesson_ad.price)
     corgi = {
         "title": "Вельш-корги",
@@ -53,6 +66,6 @@ if __name__ == '__main__':
             "address": "сельское поселение Ельдигинское, поселок санатория Тишково, 25"
         }
     }
-    corgi_ad = Advert()
-    corgi_ad.adder(corgi)
-    print(corgi_ad)
+    corgi_ad = Advert(corgi)
+    corgi_ad.price = -1
+    print(corgi_ad.class_)
